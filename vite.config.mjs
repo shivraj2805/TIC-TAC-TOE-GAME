62925956fa75a1bb1a66ab4da58d7554c00746d0
shivraj2805 <darekar1005@gmail.com>
2025-03-14 21:47:56 +0530

    Added all tic tac toe game files

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/minimax-game-ai/',
-})
+  server: {
+    port: 8080,   // Ensures Render detects the port
+    host: true    // Allows external access (0.0.0.0)
+  },
+  preview: {
+    port: 8080,   // Ensure preview uses the correct port
+    host: true
+  }
+});
